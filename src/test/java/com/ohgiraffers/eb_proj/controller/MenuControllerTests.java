d9f73f19c78516cc29f8410aa358585a479043fe
1etterh <1etterh.dev@gmail.com>
2024-11-05 12:15:27 +0900

    secret key

--- src/test/java/com/ohgiraffers/eb_proj/controller/MenuControllerTests.java
+++ src/test/java/com/ohgiraffers/eb_proj/controller/MenuControllerTests.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import javax.print.attribute.standard.Media;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,5 +29,15 @@ class MenuControllerTests {
                 .andExpect(status().isOk())
                 .andDo(print());
 
+    }
+
+    @DisplayName("2번 메뉴 확인")
+    @Test
+    public void findMenuByCodeTest() throws Exception {
+        mockMvc.perform(get("/menus/2"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.menuName").value("우럭스무디"))
+                .andDo(print());
     }
 }
